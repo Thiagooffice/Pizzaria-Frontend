0894a3814f2becf07f7d013b5f14f9be950bb48a
thiago <thiagooffice2@gmail.com>
2022-05-25 19:53:19 -0300

    product screen

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -5,6 +5,8 @@ import { useState, FormEvent } from "react"
 import { setupAPIClient } from "../../services/api"
 import { toast } from "react-toastify"
 
+import {canSSRAuth} from '../../utils/canSSRAuth'
+
 export default function Category(){
 
     const [name, setName] = useState("")
@@ -53,3 +55,11 @@ export default function Category(){
         </>
     )
 }
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+
+    return {
+        props:{}
+    }
+
+})
